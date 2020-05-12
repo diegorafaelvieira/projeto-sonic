11e956c1d3265cd36d95791fdf01dd8d9e757c03
Diego Rafael Vieira <diegorafaelvieira@bol.com.br>
2020-05-11 23:20:47 -0300

    responsividade + slider

---
+++ js/script.js
@@ -0,0 +1,35 @@
+if(window.SimpleSlide) {
+
+    new SimpleSlide({
+        slide: "quote", // nome do atributo data-slide="principal"
+        time: 5000 // tempo de transição dos slides
+    });
+
+    new SimpleSlide({
+        slide: "screen1", // nome do atributo data-slide="principal"
+        time: 5000, // tempo de transição dos slides
+        nav: true // se deve ou não mostrar a navegação
+    });
+    new SimpleSlide({
+        slide: "screen2", // nome do atributo data-slide="principal"
+        time: 5000, // tempo de transição dos slides
+        nav: true // se deve ou não mostrar a navegação
+    });
+    new SimpleSlide({
+        slide: "screen3", // nome do atributo data-slide="principal"
+        time: 5000, // tempo de transição dos slides
+        nav: true // se deve ou não mostrar a navegação
+    });
+    new SimpleSlide({
+        slide: "screen4", // nome do atributo data-slide="principal"
+        time: 5000, // tempo de transição dos slides
+        nav: true // se deve ou não mostrar a navegação
+    });
+
+}
+
+if(window.SimpleAnime) {
+
+    new SimpleAnime();
+    
+}
